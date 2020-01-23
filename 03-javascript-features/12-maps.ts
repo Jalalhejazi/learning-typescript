b2ce41de8ff9cb1b846c3d60892ec2ed3e9bb7fa
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-01-23 11:06:06 +0000

    Updated 12-maps.ts

--- 03-javascript-features/12-maps.ts
+++ 03-javascript-features/12-maps.ts
@@ -1,3 +1,12 @@
-// TODO
+// Ref
+const todo = "https://scotch.io/tutorials/4-uses-of-javascripts-arraymap-you-should-know"
 
-let read = "https://scotch.io/tutorials/4-uses-of-javascripts-arraymap-you-should-know"
+
+
+const sweetArray = [2, 3, 4, 5, 35]
+const sweeterArray = sweetArray.map(sweetItem => {
+    return sweetItem * 2
+})
+
+console.log(sweeterArray) // [4, 6, 8, 10, 70]
+
