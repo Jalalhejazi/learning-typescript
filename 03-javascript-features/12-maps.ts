2e40726d1139e7f367e7a25a6e69cce8e6a05153
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-01-23 14:05:24 +0000

    Updated 12-maps.ts

--- 03-javascript-features/12-maps.ts
+++ 03-javascript-features/12-maps.ts
@@ -1,12 +1,28 @@
-// Ref
-const todo = "https://scotch.io/tutorials/4-uses-of-javascripts-arraymap-you-should-know"
-
-
 
 const sweetArray = [2, 3, 4, 5, 35]
 const sweeterArray = sweetArray.map(sweetItem => {
     return sweetItem * 2
 })
-
 console.log(sweeterArray) // [4, 6, 8, 10, 70]
+
+
+
+
+
+
+
+
+
+
+
+const myUsers = [
+    { name: 'chuloo', likes: 'grilled chicken' },
+    { name: 'chris', likes: 'cold beer' },
+    { name: 'sam', likes: 'fish biscuits' }
+]
+const usersByFood = myUsers.map(item => {
+    const container = {}
+    container[item.name] = item.likes
+    return container
+})
 
