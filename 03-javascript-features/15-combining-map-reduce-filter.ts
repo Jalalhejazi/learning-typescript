ee01f65d9c6426036d6238bc99ac11bb12c4c46c
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-01-24 12:27:31 +0000

    Updated 15-combining-map-reduce-filter.ts

--- 03-javascript-features/15-combining-map-reduce-filter.ts
+++ 03-javascript-features/15-combining-map-reduce-filter.ts
@@ -2,3 +2,58 @@
     Combining .map(), .reduce(), and .filter()
     Since all three are called on arrays and since .map() and .filter() both return arrays, we can easily chain our calls.
  */
+
+
+const personnel = [
+  {
+    id: 5,
+    name: "Luke Skywalker",
+    pilotingScore: 98,
+    shootingScore: 56,
+    isForceUser: true,
+  },
+  {
+    id: 82,
+    name: "Sabine Wren",
+    pilotingScore: 73,
+    shootingScore: 99,
+    isForceUser: false,
+  },
+  {
+    id: 22,
+    name: "Zeb Orellios",
+    pilotingScore: 20,
+    shootingScore: 59,
+    isForceUser: false,
+  },
+  {
+    id: 15,
+    name: "Ezra Bridger",
+    pilotingScore: 43,
+    shootingScore: 67,
+    isForceUser: true,
+  },
+  {
+    id: 11,
+    name: "Caleb Dume",
+    pilotingScore: 71,
+    shootingScore: 85,
+    isForceUser: true,
+  },
+]
+
+
+
+// Our objective: get the total score of force users only (isForceUser: true)
+const totalScore = personnel
+  .filter(person => person.isForceUser)
+  .map(jedi => jedi.pilotingScore + jedi.shootingScore)
+  .reduce((acc, score) => acc + score, 0);
+
+
+console.log(totalScore) // 420
+
+
+
+
+
