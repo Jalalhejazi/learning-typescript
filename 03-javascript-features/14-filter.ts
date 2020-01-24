da1876178329c5161a289ce947a8c497c7f0ed7d
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-01-24 12:17:11 +0000

    Added 14-filter.ts

---
+++ 03-javascript-features/14-filter.ts
@@ -0,0 +1,36 @@
+/*
+    What if you have an array, but only want some of the elements in it? 
+    That’s where .filter() comes in!
+*/
+
+let pilots = [
+  {
+    id: 2,
+    name: "Wedge Antilles",
+    faction: "Rebels",
+  },
+  {
+    id: 8,
+    name: "Ciena Ree",
+    faction: "Empire",
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire",
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels",
+  }
+]
+
+
+// Say we want two arrays now: one for rebel pilots, the other one for imperials. With .filter() it couldn’t be easier!
+
+const rebels = pilots.filter(pilot => pilot.faction === "Rebels")
+
+const empire = pilots.filter(pilot => pilot.faction === "Empire")
+
+
