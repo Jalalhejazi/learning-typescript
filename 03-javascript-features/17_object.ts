1a6dabc71ed444c5d37d0b6aeffab2b41ab35edf
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-10-20 09:41:50 +0000

    Added 17_object.ts

---
+++ 03-javascript-features/17_object.ts
@@ -0,0 +1,10 @@
+var user = {
+    name : "Jalal",
+    gender : "👨‍⚕️",
+    job  : "DevOps Engineer"
+}
+Object.keys(user)       // ["name", "gender", "job"]
+
+Object.values(user)     // ["Jalal", "👨‍⚕️", "DevOps Engineer"]
+
+Object.entries(user) 
