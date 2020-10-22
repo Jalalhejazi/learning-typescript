139556a7a04d80c6f48996a9dedd12e311076e16
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-10-22 09:36:32 +0000

    Updated server.js

--- 100-images/program/server.js
+++ 100-images/program/server.js
@@ -2,28 +2,29 @@
 // 
 // TODO: use modern EcmaScript Language Feature to make this program more 2020 friendly
 // - remove semi-colons
-// - use const or let instead of var
+// - use const or let instead of const
 // - use arrow-function => instead of function(){}
 // - use TemplateString 
 // - What if you rename the file from server.js to server.ts  :-) 
 // - etc......
 
+const port = 1234
 
-var http = require('http'),
+const http = require('http'),
     fs = require('fs'),
     path = require('path'),
     url = require('url');
     imageDir = '../';
  
 
-http.createServer(function (req, res) {
-    var query = url.parse(req.url,true).query;
+http.createServer( (req, res) => {
+    const query = url.parse(req.url,true).query;
         pic = query.image;
  
     if (typeof pic === 'undefined') {
-        getImages(imageDir, function (err, files) {
-            var imageLists = '<ul>';
-            for (var i=0; i<files.length; i++) {
+        getImages(imageDir,  (err, files) => {
+            let imageLists = '<ul>';
+            for (let i=0; i<files.length; i++) {
                 imageLists += '<li><a href="/?image=' + files[i] + '">' + files[i] + '</li>';
             }
             imageLists += '</ul>';
@@ -32,7 +33,7 @@ http.createServer(function (req, res) {
         });
     } else {
         //read the image using fs and send the image content back in the response
-        fs.readFile(imageDir + pic, function (err, content) {
+        fs.readFile(imageDir + pic, (err, content) => {
             if (err) {
                 res.writeHead(400, {'Content-type':'text/html'})
                 console.log(err);
@@ -45,14 +46,14 @@ http.createServer(function (req, res) {
         });
     }
  
-}).listen(3333);
-console.log("Server running at http://localhost:3333/");
+}).listen(port);
+console.log(`Server running at http://localhost:${port}/`);
  
 //get the list of jpg files in the image dir
-function getImages(imageDir, callback) {
-    var fileType = '.jpg',
+let getImages = (imageDir, callback) => {
+    let fileType = '.jpg',
         files = [], i;
-    fs.readdir(imageDir, function (err, list) {
+    fs.readdir(imageDir, (err, list) => {
         for(i=0; i<list.length; i++) {
             if(path.extname(list[i]) === fileType) {
                 files.push(list[i]); //store the file name into the array files
